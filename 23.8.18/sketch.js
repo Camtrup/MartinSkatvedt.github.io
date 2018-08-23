f88d26827ee6ec68bd23c7dc3f48ea3d2d462e52
MartinSkatvedt <32820275+MartinSkatvedt@users.noreply.github.com>
2018-08-23 10:45:28 +0200

    Sortert

--- 23.8.18/sketch.js
+++ 23.8.18/sketch.js
@@ -1,7 +1,7 @@
 
 
 function buttonClick() {
-  fetch("https://cors-anywhere.herokuapp.com/http://worldclockapi.com/api/json/utc/now")
+  fetch("https://cors-anywhere.herokuapp.com/http://worldclockapi.com/api/json/utc/now", {'mode': 'no-cors'})
   .then(response => response.json())
   .then(function(myJson) {
     let text = myJson.dayOfTheWeek;
