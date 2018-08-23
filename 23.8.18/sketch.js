b7ecde3b2d8e5513ef5e8a0f5dac223f9df77d19
MartinSkatvedt <32820275+MartinSkatvedt@users.noreply.github.com>
2018-08-23 10:20:55 +0200

    Sortert

---
+++ 23.8.18/sketch.js
@@ -0,0 +1,11 @@
+
+
+function buttonClick() {
+  fetch("http://worldclockapi.com/api/json/utc/now")
+  .then(response => response.json())
+  .then(function(myJson) {
+    let text = myJson.dayOfTheWeek;
+    console.log(`Today is: ${text}`);
+    document.getElementById('para').innerText = text;
+  });
+}
