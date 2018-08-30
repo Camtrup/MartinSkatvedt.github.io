6055192f8cc20afc0ae447b10eac3c837b86722a
MartinSkatvedt <32820275+MartinSkatvedt@users.noreply.github.com>
2018-08-31 00:03:17 +0200

    cleaned js

--- 30.8.18/sketch.js
+++ 30.8.18/sketch.js
@@ -1,16 +1,13 @@
-let arial, omkrets;
 
 function nameClick() {
   console.log("Jeg heter Martin!");
 }
 
 function sumbitClick() {
-  let høyde = document.getElementById("h").value
-  let bredde = document.getElementById("b").value
-
-
-  arial = kalkArial(høyde, bredde);
-  omkrets = kalkOmkrets(høyde, bredde);
+  let høyde = Number(document.getElementById("h").value);
+  let bredde = Number(document.getElementById("b").value);
+  let arial = høyde * bredde;
+  let omkrets = (høyde * 2) + (bredde* 2);
 
   if(isNaN(omkrets) || isNaN(bredde)) {
     alert("Data må kun være tall!")
@@ -18,12 +15,4 @@ function sumbitClick() {
   else {
     alert(`Arialet er ${arial}, og omkretsen er ${omkrets}`);
   }
-}
-
-function kalkArial(høyde, bredde) {
-  return (høyde * bredde);
-}
-
-function kalkOmkrets(høyde, bredde) {
-  return ((høyde * 2) + (bredde* 2));
 }
